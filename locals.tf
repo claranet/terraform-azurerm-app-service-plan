2a8707d5c7c241e792e9f490c5bd30a51a5caa97
Rémi Beaufils <remi.beaufils@fr.clara.net>
2024-11-07 17:24:52 +0100

    fix: zone_balancing compatibles SKU

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  zone_balancing_enabled = var.sku_name == "Y1" ? false : var.zone_balancing_enabled
+  zone_balancing_enabled = contains(["Y1", "B1", "B2", "B3", "S1", "S2", "S3"], var.sku_name) ? false : var.zone_balancing_enabled
   worker_count           = var.sku_name == "Y1" ? null : var.worker_count
 }
